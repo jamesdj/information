dd2a1ecfa7696a299097a0bd8c2d048526bc1956
James Jensen <james.jensen@James-Jensens-MacBook-Pro.local>
2018-06-06 13:42:49 -0600

    enable importing from higher level

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .information import compute_mutual_information, compute_ic, Revealer
+__all__ = ['compute_mutual_information', 
+           'compute_ic', 
+           'Revealer']
---
+++ information/__init__.py
@@ -0,0 +1,6 @@
+from .information import compute_mutual_information, compute_ic
+from .revealer import Revealer
+
+__all__ = ['compute_mutual_information',
+           'compute_ic',
+           'Revealer']
